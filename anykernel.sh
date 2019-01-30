a75ecc3eeacc56be6e4cdb596ea9a9337d472c45
Henrique Pereira <pereirah2001@gmail.com>
2019-01-30 20:50:34 -0200

    Initial commit
    Signed-off-by: Henrique Pereira <pereirah2001@gmail.com>

---
+++ anykernel.sh
@@ -0,0 +1,43 @@
+# AnyKernel2 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() {
+kernel.string=
+do.devicecheck=1
+do.modules=0
+do.cleanup=0
+do.cleanuponabort=0
+} # end properties
+
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=0;
+ramdisk_compression=auto;
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. /tmp/anykernel/tools/ak2-core.sh;
+
+
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+chmod -R 750 $ramdisk/*;
+chown -R root:root $ramdisk/*;
+
+
+## AnyKernel install
+dump_boot;
+
+## begin ramdisk changes
+
+# add floppy script
+insert_line init.rc "import /init.kernelx.rc" before "import /init.environ.rc" "import /init.kernelx.rc";
+
+## end ramdisk changes
+
+write_boot;
+
+## end install
